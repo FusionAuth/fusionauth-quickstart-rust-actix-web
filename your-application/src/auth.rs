f8caf8a522a62da830e8fea1b7137ebb7c6dfb2f
RichardJECooke <RichardJECooke@pm.me>
2023-12-30 09:10:48 +0200

    got async to work

--- your-application/src/auth.rs
+++ your-application/src/auth.rs
@@ -47,33 +47,31 @@ struct AuthCallbackParams {
     code: String,
 }
 
-//Result<HttpResponse, Error>
 #[get("/callback")]
-async fn callback(params: web::Query<AuthCallbackParams>, session: Session) ->  impl Responder {
+async fn callback(params: web::Query<AuthCallbackParams>, session: Session) ->  Result<HttpResponse, Error> {
     //callback url from fusionauth: http://localhost:9012/callback?code=4u73uE6d-xbJ5lTBeY2z7cCXMWMEYP_TSB7v_vOdJp4&locale=en&state=XPNMGQfsHPKNP7PK7qyJyg&userState=Authenticated
     let received_state = &params.state;
     if let Ok(saved_state) = session.get::<String>("csrf_token") {
         if saved_state != Some(received_state.clone()) {
-            return HttpResponse::BadRequest().body("PKCE state mismatch");
+            return Ok(HttpResponse::BadRequest().body("PKCE state mismatch"));
         }
     }
     else {
-        return HttpResponse::InternalServerError().body("Session error")
+        return Ok(HttpResponse::InternalServerError().body("Session error"));
     }
     let pkce_verifier = session.get::<String>("pkce_verifier").unwrap().unwrap();
-    let token_result = get_oauth_client()
+    let token_result = match get_oauth_client()
         .exchange_code(AuthorizationCode::new(params.code.clone()))
         .set_pkce_verifier(PkceCodeVerifier::new(pkce_verifier))
-        // .request(http_client);
-        .request_async(async_http_client).await?;
-    // {
-    //     Ok(result) => result,
-    //     Err(e) => {
-    //         return Ok(HttpResponse::InternalServerError().body("Internal server error"));
-    //     }
-    // };
+        .request_async(async_http_client)
+        .await {
+            Ok(result) => result,
+            Err(e) => {
+                return Ok(HttpResponse::InternalServerError().body("Error during token exchange"));
+            }
+        };
     // session.insert("email", token_result.email.to_string().unwrap());
-    HttpResponse::Found().append_header(("Location", "/account")).finish()
+    Ok(HttpResponse::Found().append_header(("Location", "/account")).finish())
 }
 
 fn get_oauth_client() -> BasicClient {
